5a6bf568f25a67e8d89f4cdfd77f864fa316b504
Laura Concepción - Usabi <info@usabi.es>
2013-01-15 10:01:19 +0000

    Primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gfp::Application.config.secret_token = '5f5d46fae70423b93b6a47256295138e2f2521a5a4f7ffb058c7ad465c28ad45c8ebe254ab8e99f4e770bf0b5c9fc8514452a1486b7541b422b003c5cdd17560'
