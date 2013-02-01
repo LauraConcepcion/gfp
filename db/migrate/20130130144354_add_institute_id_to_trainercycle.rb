bea456386abca977d7de7563061c04a37b33016e
user <user@ubuntu.localdomain>
2013-02-01 09:01:39 -0500

    error integer_id type trainercicle

--- db/migrate/20130130144354_add_institute_id_to_trainercycle.rb
+++
@@ -1,5 +0,0 @@
-class AddInstituteIdToTrainercycle < ActiveRecord::Migration
-  def change
-    add_column :trainercycles, :institute_id, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
- encoding: UTF-8
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130129204627) do
+ActiveRecord::Schema.define(:version => 20130201140012) do
 
   create_table "aacces", :force => true do |t|
     t.string   "name"
@@ -75,7 +75,7 @@ ActiveRecord::Schema.define(:version => 20130129204627) do
     t.string   "edition"
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
-    t.string   "institute_id"
+    t.integer  "institute_id"
   end
 
   create_table "users", :force => true do |t|
