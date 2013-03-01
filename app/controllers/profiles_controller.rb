952997a9fdb490882171c592f66fa3e676112898
user <user@ubuntu.localdomain>
2013-03-01 07:30:51 -0500

    modificacion del edit y guardar de profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,9 +2,23 @@ class ProfilesController < InheritedResources::Base
   before_filter :authenticate_teacher!  
   respond_to :html, :xml, :json
   def new
+    debugger
     @profile = Profile.new
     @teacher = current_teacher
   end
+  def edit 
+    @profile = Profile.find(params[:id])
+    @teacher = current_teacher
+  end
   def create
+    debugger
+    @profile = Profile.new(params[:profile])
+    @profile.teacher = current_teacher
+    if @profile.save
+      flash[:notice] = "Succesfully registered"
+      redirect_to teacher_profile_path(current_teacher.id,@profile.id)
+    else
+        render :action => 'new'
+    end
   end
 end
