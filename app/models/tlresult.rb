284d659fdbfe2888a62289010e94320d98a21c17
user <user@ubuntu.localdomain>
2013-03-22 15:10:34 -0400

    quitar doble belong_to y traducciones de admin

--- app/models/tlresult.rb
+++ app/models/tlresult.rb
@@ -1,6 +1,5 @@
 class Tlresult < ActiveRecord::Base
-  attr_accessible :name, :criterion_ids, :contentblock_id
-  has_one :contentblock
+  attr_accessible :name, :criterion_ids
   has_many :qualifyingentity_tlresults
   has_many :criterions
 end
