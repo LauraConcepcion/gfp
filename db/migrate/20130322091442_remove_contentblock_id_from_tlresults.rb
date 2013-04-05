dce0595c879e99698e240b7a302216f4b63aeb8b
Laura Concepción - Usabi <info@usabi.es>
2013-04-05 21:19:22 +0100

    borramos migración

--- db/migrate/20130322091442_remove_contentblock_id_from_tlresults.rb
+++
@@ -1,9 +0,0 @@
-class RemoveContentblockIdFromTlresults < ActiveRecord::Migration
-  def up
-    remove_column :tlresults, :contentblock_id
-  end
-
-  def down
-    add_column :tlresults, :contentblock_id, :integer
-  end
-end
