50517479355713fae9d1ea5d27fb69b992860bf0
Laura Concepción - Usabi <info@usabi.es>
2013-04-13 10:06:02 +0100

    Correccion sobre la nueva columna creada anterior commit

--- app/models/institute.rb
+++ app/models/institute.rb
@@ -1,5 +1,5 @@
 class Institute < ActiveRecord::Base
-  attr_accessible :name, :address, :aacce_id, :phone, :email, :web, :cec, :locality, :zip, :city, :province, :fax, :holder, :type, :nature, :island, :code_import, :code_center, :accee_code, :com
+  attr_accessible :name, :address, :aacce_id, :phone, :email, :web, :cec, :locality, :zip, :city, :province, :fax, :holder, :type, :nature, :island, :code_import, :code_center, :aacce_code, :com
   validates :name, :address, :code_import, :presence => true
   #validates :email, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }
   #validates :web, :format => {:with => /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/}
