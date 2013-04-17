f46422d91fac2e184b02dc4319d3d6cd81568064
user <user@ubuntu.localdomain>
2013-04-17 08:16:04 -0400

    combos anidados en profile

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,43 +1,34 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
+// This is a manifest file that'll be compiled into including all the files listed below.
+// Add new JavaScript/Coffee code in separate files in this directory and they'll automatically
+// be included in the compiled file accessible from http://example.com/assets/application.js
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // the compiled file.
 //
-// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
-// GO AFTER THE REQUIRES BELOW.
-//
 //= require jquery
 //= require jquery_ujs
-//= require jquery-ui
 //= require twitter/bootstrap
 //= require_tree .
-
+//
 $(document).ready(function(){
   $("#profile_institute_id").change(function() {
-      var provenance = $('#profile_institute_id');
+    var provenance = $('#profile_institute_id');
       $.getJSON('/institutes/update_trainercycle/' + provenance.val() || "0", function(trainercycles) {
-            var options = ''; 
-            for(var i = 0; i < trainercycles.length; i++)
-            options += '<option value="' + trainercycles[i][1] +'">' +  trainercycles[i][0]+ '</option>';
-          $("select#profile_trainercycle_id").html(options);
-          });
+        var options = '';
+        for(var i = 0; i < trainercycles.length; i++)
+          options += '<option value="' + trainercycles[i][1] +'">' + trainercycles[i][0]+ '</option>';
+        $("select#profile_trainercycle_id").html(options);
+      });
       return false;
-    });
+  });
 
   $("#profile_trainercycle_id").change(function() {
-      var provenance = $('#profile_trainercycle_id');
-      $.getJSON('/trainercycles/update_matter/' + provenance.val() || "0", function(matters) {
-            var options = ''; 
-            for(var i = 0; i < matters.length; i++)
-            options += '<option value="' + matters[i][1] +'">' +  matters[i][0]+ '</option>';
-          $("select#profile_matter_id").html(options);
-          });
-      return false;
+    var provenance = $('#profile_trainercycle_id');
+    $.getJSON('/trainercycles/update_matter/' + provenance.val() || "0", function(matters) {
+      var options = '';
+      for(var i = 0; i < matters.length; i++)
+        options += '<option value="' + matters[i][1] +'">' + matters[i][0]+ '</option>';
+      $("select#profile_matter_id").html(options);
     });
-
+    return false;
+  });
 });
-
