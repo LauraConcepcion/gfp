03ba2c0a49b6c602dc51233a4515ec8ab7a73a3f
Usabi <usabi@MacBook-Pro-de-Usabi.local>
2013-06-15 13:33:57 +0100

    Estabilizamos perfil

--- app/models/institute.rb
+++ app/models/institute.rb
@@ -4,8 +4,8 @@ class Institute < ActiveRecord::Base
   #validates :email, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }
   #validates :web, :format => {:with => /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/}
   belongs_to  :aacce
+  has_many :profiles
   #has_many :departments
   #attr_accessible :department_ids
-  #has_many :trainercycles
   #attr_accessible :trainercycle_ids
 end
