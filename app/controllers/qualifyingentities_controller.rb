883dd41e4579537741e63c02a3b7827ecf5d5b37
Ignacio Aliende García <ialiendeg@gmail.com>
2013-06-16 20:24:20 +0100

    Formulario de entidad calificable añadiendo directamente reas mediante un cocoon, pudiendo especificar a la vez la ponderación. - TODO Por ahora la relación de módulo con rea es a través de bloque de contenido   pero si se decide quitar la última sólo habría que cambiar la otra y   debería funcionar.

--- app/controllers/qualifyingentities_controller.rb
+++ app/controllers/qualifyingentities_controller.rb
@@ -1,11 +1,26 @@
 class QualifyingentitiesController < InheritedResources::Base
-  before_filter :authenticate_teacher!  
+  before_filter :authenticate_teacher!
+
   respond_to :html, :xml, :json
-  def create
-    @qualifyingentity = Qualifyingentity.new(params[:qualifyingentity])
-    @qualifyingentity.profile = current_teacher.current_profile
-    create!
+
+  def new
+    @qualifyingentity = Qualifyingentity.new
+    get_profile_data
   end
+
+  def create
+    create! do |success, failure|
+      failure.html do
+        get_profile_data
+        render :new
+      end
+    end
+  end
+
+  def edit
+    get_profile_data
+  end
+
   def put_scores
     @students = Student.student_list(current_teacher.current_profile.group_id,current_teacher.current_profile.matter_id,current_teacher.current_profile.trainercycle_id)
     @qualifyingentity = Qualifyingentity.find(params[:qualifyingentity_id])
@@ -17,10 +32,27 @@ class QualifyingentitiesController < InheritedResources::Base
     #@qualifyingentity.students = @students
     debugger
   end
+
   private
+
   def collection
     @q ||= Qualifyingentity.accessible_by(current_ability).for_profile(current_teacher.current_profile).search(params[:q])
     @q.sorts = "name asc" if @q.sorts.empty?
     @qualifyingentities||= @q.result(:distintct => true).page(params[:page])
+  end
+
+  # TODO La parte de current_profile igual va mejor en application_controller
+  # El problema es que la asignación de profile al resource peta si no se inicializa antes y en el before_filter no lo está
+  def get_profile_data
+    @current_profile = current_teacher.current_profile if current_teacher
+    unless @current_profile
+      # Por si no existen profiles dados de alta
+      flash[:error] = t(:no_current_profile)
+      redirect_to :root_path
+      return
+    end
+    @qualifyingentity.profile ||= @current_profile
+    @matter = @current_profile.matter
+    @tlresults = @matter.tlresults if @matter
   end
 end
--- app/models/qualifyingentity.rb
+++ app/models/qualifyingentity.rb
@@ -1,10 +1,11 @@
 class Qualifyingentity < ActiveRecord::Base
-  attr_accessible :name, :classroom_id, :contentblock_ids, :qualifyingentity_criterion_ids, :qualifyingentity_tlresults_attributes, :scores_attributes
+  attr_accessible :name, :profile_id, :qualifyingentity_tlresults_attributes
   
   #Mediante profile se determina la asignatura para la que se están creando las entidades calificables
   belongs_to :profile
   #Relación con los tlresult y sus porcentajes
   has_many :qualifyingentity_tlresults
+  has_many :tlresults, :through => :qualifyingentity_tlresults
 
   has_many :scores
   #FIXME: queda pendiente determinar las relaciones con los alumnos
@@ -18,11 +19,14 @@ class Qualifyingentity < ActiveRecord::Base
   #accepts_nested_attributes_for :qualifyingentity_criterions, :reject_if => :all_blank, :allow_destroy => true 
   accepts_nested_attributes_for :qualifyingentity_tlresults, :reject_if => :all_blank, :allow_destroy => true
   accepts_nested_attributes_for :scores
+
   #  before_destroy :can_be_destroyed?
-  after_create :create_qualifyingentity_tlresults
+  after_save :create_qualifyingentity_tlresults
+
   validates :name, :presence => true
   validates :profile_id, :presence => true
-  validates :scores_id, :presence => true
+  #validates :score_ids, :presence => true
+
   scope :for_profile, lambda {|profile|
     where("profile_id = ?", profile.id) if profile
   }
@@ -33,13 +37,13 @@ class Qualifyingentity < ActiveRecord::Base
   
   def teacher
     unless self.classroom.nil?
-      self.classrrom.teacher
+      self.classroom.teacher
     end
   end
 
   def create_qualifyingentity_tlresults
     self.contentblocks.each do |cb|
-      self.qualifyingentity_tlresults << QualifyingentityTlresult.new(:percentage => 0, :tlresult_id => cb.tlresult.id)
+      self.qualifyingentity_tlresults.build(:percentage => 0, :tlresult_id => cb.tlresult.id) unless tlresult_ids.include?(tlresult.id)
       self.save
     end
   end
