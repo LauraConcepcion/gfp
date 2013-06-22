88924765550698557195a91772575ffac5a17125
Ignacio Aliende García <ialiendeg@gmail.com>
2013-06-22 10:14:30 +0100

    Pasamos detalle de REAs de entidad calificable a cocoon con tabla para unificar diseño.

--- app/models/matter.rb
+++ app/models/matter.rb
@@ -2,7 +2,6 @@ class Matter < ActiveRecord::Base
   attr_accessible :curso, :name, :contentblock_ids, :trainercycle_id, :m_id
   has_many :profiles
   has_many :contentblocks
-  # FIXME Por ahora los asociamos a través de contentblocks pero luego serán directos seguramente
   has_many :tlresults
   belongs_to :trainercycle
   validates :name , :presence => true
