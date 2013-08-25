f8194376b92a24b9005a7648d981daebd3f3a980
GUEBS <guebs@usabi.es>
2013-08-25 20:20:53 +0200

    Quitamos la conversión a fechas.

--- app/models/incident.rb
+++ app/models/incident.rb
@@ -4,6 +4,6 @@ class Incident < ActiveRecord::Base
   belongs_to :profile
 
   scope :for_profile_and_student_and_quarter, lambda {|profile, student, quarter|
-    where('profile_id = ? and student_id = ? and incident_datetime::date >= ? and incident_datetime::date < ?', profile.id, student.id, quarter.start_date, quarter.end_date)
+    where('profile_id = ? and student_id = ? and incident_datetime >= ? and incident_datetime < ?', profile.id, student.id, quarter.start_date, quarter.end_date)
   }
 end
