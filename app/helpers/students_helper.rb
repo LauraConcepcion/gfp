14a5b12b81b35d71493cb9f358ae2cd1f3356171
Ignacio Aliende García <ialiendeg@gmail.com>
2013-09-01 18:25:51 +0100

    Arreglamos errores.

--- app/helpers/students_helper.rb
+++ app/helpers/students_helper.rb
@@ -1,2 +1,7 @@
 module StudentsHelper
+  def classroom_name_and_profile(classroom)
+    return t(:no_classroom) unless classroom
+
+    raw((classroom.name || t(:no_name)) + " / " + (classroom.profile ? (classroom.profile.name || t(:name)) : t(:no_profile_for_classroom)))
+  end
 end
