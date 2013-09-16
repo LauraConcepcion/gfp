5553845461019a8f4db34e8ead3a5a10612602af
Ignacio Aliende García <ialiendeg@gmail.com>
2013-09-16 18:23:50 +0100

    Correcciones en las medias.

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -7,7 +7,9 @@ module ProfilesHelper
 
   def average_for(student, tlr, quarter_id, profile_id)
     average_score = AverageScore.where(:student_id => student.id, :tlresult_id => tlr.try(:id), :quarter_id => quarter_id, :profile_id => profile_id, :teacher_revised => false).first
-    average_score ? number_with_precision(average_score.grade, :precision => 2) : '-'
+    value = average_score.try(:grade) ? number_with_precision(average_score.grade, :precision => 2) : '-'
+    value = value.gsub(/[,.]00$/, '').gsub(/(\d+[,.]\d)0$/, '\1')
+    value
   end
 
   def even_odd(count)
