e6222630ce39bfced904d581b5ecd037321290ee
Laura Concepción Rodríguez <usabi@MacBook-Pro-de-Laura.local>
2013-09-17 12:14:59 +0100

    Tarea #83 Libro de calificaciones

--- db/schema.rb
+++ db/schema.rb
@@ -99,6 +99,7 @@ ActiveRecord::Schema.define(:version => 20130912174506) do
     t.string   "name"
     t.datetime "created_at",        :null => false
     t.datetime "updated_at",        :null => false
+    t.integer  "institute_id"
     t.integer  "aacce_id"
     t.integer  "departmenttype_id"
   end
@@ -232,7 +233,7 @@ ActiveRecord::Schema.define(:version => 20130912174506) do
   create_table "qualifyingentity_tlresults", :force => true do |t|
     t.integer  "qualifyingentity_id"
     t.integer  "tlresult_id"
-    t.float    "percentage"
+    t.integer  "percentage"
     t.datetime "created_at",          :null => false
     t.datetime "updated_at",          :null => false
     t.integer  "profile_id"
@@ -294,14 +295,14 @@ ActiveRecord::Schema.define(:version => 20130912174506) do
   create_table "teachers", :force => true do |t|
     t.string   "name"
     t.string   "surname"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.datetime "created_at",                                            :null => false
+    t.datetime "updated_at",                                            :null => false
+    t.string   "email",                                 :default => "", :null => false
+    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
+    t.integer  "sign_in_count",                         :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -309,11 +310,9 @@ ActiveRecord::Schema.define(:version => 20130912174506) do
     t.string   "teacher_code"
     t.string   "dni"
     t.string   "phone"
-    t.string   "confirmation_token"
-    t.datetime "confirmation_sent_at"
+    t.datetime "confirmed_at"
   end
 
-  add_index "teachers", ["confirmation_token"], :name => "index_teachers_on_confirmation_token", :unique => true
   add_index "teachers", ["email"], :name => "index_teachers_on_email", :unique => true
   add_index "teachers", ["reset_password_token"], :name => "index_teachers_on_reset_password_token", :unique => true
 
@@ -330,9 +329,8 @@ ActiveRecord::Schema.define(:version => 20130912174506) do
 
   create_table "tlresults", :force => true do |t|
     t.text     "name"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
-    t.integer  "contentblock_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
     t.integer  "matter_id"
     t.string   "rayce_id"
     t.integer  "profile_id"
@@ -357,18 +355,18 @@ ActiveRecord::Schema.define(:version => 20130912174506) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "email",                                 :default => "", :null => false
+    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
+    t.integer  "sign_in_count",                         :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at",                                            :null => false
+    t.datetime "updated_at",                                            :null => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
